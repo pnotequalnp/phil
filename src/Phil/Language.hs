0bde8bb607b88ff7f077c47d30ef62f1c0cd49b0
Kevin Mullins <kevin@pnotequalnp.com>
2021-10-12 13:35:22 -0700

    Add basic frontend

--- src/Phil/Language.hs
+++ src/Phil/Language.hs
@@ -1,1 +1,37 @@
 module Phil.Language where
+
+import Data.Text (Text)
+
+type Name = Text
+
+data PLit
+  = LInt Integer
+  | LFloat Double
+  | LString Text
+  | LBool Bool
+  deriving stock (Show)
+
+data PExpr
+  = EVar Name
+  | EApp PExpr PExpr
+  | ELam Name PExpr
+  | ELet Name PExpr PExpr
+  | ELit PLit
+  deriving stock (Show)
+
+newtype TVar = TV Name
+  deriving stock (Show)
+  deriving newtype (Eq, Ord)
+
+newtype TyCon = TC Name
+  deriving stock (Show)
+  deriving newtype (Eq, Ord)
+
+data PType
+  = TVar TVar
+  | TCon TyCon
+  | PType :>> PType
+  | TForall [TVar] PType
+  deriving stock (Eq, Ord, Show)
+
+infixr 5 :>>
