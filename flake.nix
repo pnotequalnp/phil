1b90253efd386b46fb7b9985489ce480c0e685c5
Kevin Mullins <kevin@pnotequalnp.com>
2021-10-13 14:38:03 -0700

    Fix cabal file and flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in rec {
       devShell.x86_64-linux = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; with haskellPackages; [ alex happy ghcid zlib ];
+        nativeBuildInputs = with pkgs; with haskellPackages; [ alex happy ghcid pkgs.zlib ];
       };
     };
 }
